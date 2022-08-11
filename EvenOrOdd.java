83a12bd9ea0ca4af9e84f7e569178a662b9b82f7
JSPICELA <jerrod.spiceland@mykenzie.snhu.edu>
2022-08-11 16:07:11 -0500

    First commit

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,7 +1,7 @@
 public class EvenOrOdd {
     public static void main(String[] args) {
 
-        for (int i = 0; i<20; i++ ) {
+        for (int i = 0; i<21; i++ ) {
             System.out.println();
             if (i % 2 == 0) {
                 System.out.println(i + "- even");
